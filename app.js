63b8bd1c883e72bcea22697924a6b5bf78ad2c4e
pavanmane1 <pavanmane4961@gmail.com>
2024-08-14 17:06:48 +0530

    add cors to connect react app

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const cookieParser = require("cookie-parser");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const userRoutes = require("./Routes/userRoutes");
+
 const PORT = process.env.PORT || 8080;
 const app = express();
-const userRoutes = require("./Routes/userRoutes");
+
+// Use CORS middleware before defining your routes
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow only your React app's origin
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true // If you need to allow cookies or other credentials
+}));
+
 app.use(bodyParser.json());
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // Use cookie-parser
+
+// Routes
 app.use('/', userRoutes);
 
 // Error handling middleware
@@ -15,6 +29,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
+// Start the server
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
